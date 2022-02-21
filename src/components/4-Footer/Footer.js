49cb5c7b0d86c920fb2430c4b9edd07e6d124c9c
totsuka1 <takumi.otsuka@gmail.com>
2022-02-21 18:52:20 -0500

    Social Media Footer Links

--- src/components/4-Footer/Footer.js
+++ src/components/4-Footer/Footer.js
@@ -6,7 +6,11 @@ class Footer extends React.Component {
   render() {
     return (
       <nav id="footer" className="footer-container">
-        <div className="footer-items">Takumi Otsuka 2022</div>
+        <div className="footer-items">Takumi Otsuka 2022 </div>
+        <div className="social-icons">
+          <a href={"https://www.linkedin.com/in/takumiotsuka/"}>LI</a>
+          <a href={"https://github.com/totsuka1"}>GH</a>
+        </div>
       </nav>
     );
   }
