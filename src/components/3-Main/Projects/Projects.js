a0f46a827800cc362acd78cb125ac42ae40a4e07
totsuka1 <takumi.otsuka@gmail.com>
2022-03-02 02:30:37 -0500

    responsive portfolio project cards

--- src/components/3-Main/Projects/Projects.js
+++ src/components/3-Main/Projects/Projects.js
@@ -3,6 +3,22 @@ import React from "react";
 import "./projects.css";
 
 class Projects extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      open: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(evt) {
+    if (this.state.open === evt.target.className[0]) {
+      this.setState({ open: null });
+    } else {
+      this.setState({ open: evt.target.className[0] });
+    }
+  }
+
   render() {
     return (
       <div id="projects">
@@ -12,13 +28,82 @@ class Projects extends React.Component {
           </div>
           <h4>
             //Project intro text placeholder. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit...
+            consectetur adipiscing elit... Some of the apps I've built.
           </h4>
         </div>
         <div className="projects-main-container">
-          <div className="project-container">Hello</div>
-          <div className="project-container">Hello</div>
-          <div className="project-container">Hello</div>
+          <div
+            className={
+              "1 project-container " +
+              (this.state.open === "1" ? "expanded" : "")
+            }
+            onClick={this.handleClick}
+          >
+            <div className="1 project-title">Blocks</div>
+            <div className="1 project-subtitle">
+              A Decentralized File Storage Platform
+            </div>
+            {this.state.open === "1" ? (
+              <div>
+                //Project card text and info placeholder. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit...
+              </div>
+            ) : null}
+          </div>
+          <div
+            className={
+              "2 project-container " +
+              (this.state.open === "2" ? "expanded" : "")
+            }
+            onClick={this.handleClick}
+          >
+            <div className="2 project-title">Iki</div>
+            <div className="2 project-subtitle">
+              A Mindfullness Chrome Extension
+            </div>
+            {this.state.open === "2" ? (
+              <div>
+                //Project card text and info placeholder. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit...
+              </div>
+            ) : null}
+          </div>
+          <div
+            className={
+              "3 project-container " +
+              (this.state.open === "3" ? "expanded" : "")
+            }
+            onClick={this.handleClick}
+          >
+            <div className="3 project-title">Udemy-Larks</div>
+            <div className="3 project-subtitle">
+              An E-Commerce Marketplace for Educational Videos
+            </div>
+            {this.state.open === "3" ? (
+              <div>
+                //Project card text and info placeholder. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit...
+              </div>
+            ) : null}
+          </div>
+          <div
+            className={
+              "4 project-container " +
+              (this.state.open === "4" ? "expanded" : "")
+            }
+            onClick={this.handleClick}
+          >
+            <div className="4 project-title">This Website</div>
+            <div className="4 project-subtitle">
+              My Portfolio Website, Built From the Ground Up
+            </div>
+            {this.state.open === "4" ? (
+              <div>
+                //Project card text and info placeholder. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit...
+              </div>
+            ) : null}
+          </div>
         </div>
       </div>
     );
